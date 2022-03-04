adfcc509eab1942966da5dce6b78c6fa54227198
DANILO DUARTE DE JESUS PEIXOTO <danilo.djpeixoto@senacsp.edu.br>
2022-03-03 22:03:17 -0300

    'ex01'

---
+++ Lista02/lista02/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Ex01Component } from './views/ex01/ex01.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Ex01Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
