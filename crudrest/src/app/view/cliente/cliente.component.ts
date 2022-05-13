e47d5f8faa86872a8d68f108cfc49ca6a8af7c52
DANILO DUARTE DE JESUS PEIXOTO <danilo.djpeixoto@senacsp.edu.br>
2022-05-12 22:14:44 -0300

    Aula crud

---
+++ crudrest/src/app/view/cliente/cliente.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/model/client';
+import { ClienteService } from 'src/app/service/cliente.service';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.css']
+})
+export class ClienteComponent implements OnInit {
+  clientes = new Array<Cliente>();
+  cliente?:Cliente;
+  constructor(private clienteService:ClienteService) { }
+
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  listar(){
+    this.clienteService.listar().subscribe(clientes => {
+      this.clientes = clientes;
+    });
+  }
+
+  novo(){
+    this.cliente = new Cliente();
+
+  }
+
+  salvar(){
+    if(this.cliente) {
+      this.clienteService.inserir(this.cliente).subscribe(cliente =>{
+        this.listar();
+        this.cliente = undefined;
+      })
+    }
+  }
+
+}
